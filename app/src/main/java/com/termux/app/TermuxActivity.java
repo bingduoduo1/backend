89effece5ea0c19336478daa902982015d8f852d
butub-ubuntu <1051025929@qq.com>
2019-04-13 14:42:06 +0800

    add com.google.android.material:material:1.0.0-rc01

--- app/src/main/java/com/termux/app/TermuxActivity.java
+++ app/src/main/java/com/termux/app/TermuxActivity.java
@@ -229,6 +229,10 @@ public final class TermuxActivity extends Activity implements ServiceConnection 
         mTerminalView.setKeepScreenOn(mSettings.isScreenAlwaysOn());
         mTerminalView.requestFocus();
 
+        //禁用输入法
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM,
+            WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+
         //分页
         final ViewPager viewPager = findViewById(R.id.viewpager);//androidx.viewpager.widget.ViewPager
         if (mSettings.mShowExtraKeys) viewPager.setVisibility(View.VISIBLE);
